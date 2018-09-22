437287abeae79fbeefe037967977be216669d172
Vogel <vogel@github.com>
2018-09-21 22:22:25 -0300

    Simple commits of Main Program

--- src/Program.cs
+++ src/Program.cs
@@ -7,17 +7,17 @@ namespace CompAndDel {
     class Program {
         static void Main (string[] args) {
 
-            PictureProvider img = new PictureProvider ();
-            IPicture image = img.GetPicture ("BreakingBad.jpg");
+            PictureProvider img = new PictureProvider (); 
+            IPicture image = img.GetPicture ("BreakingBad.jpg"); // "Recoge" la imagen 
 
-            IFilter negative = new FilterNegative ();
-            IFilter twitterPub = new TwitterFilter ();
+            IFilter negative = new FilterNegative (); // Creación del filtro negativo
+            IFilter twitterPub = new TwitterFilter (); //Creación del filtro de Twitter para publicar
 
-            IPipe end = new PipeNull ();
-            IPipe twitter = new PipeSerial (twitterPub, end);
-            IPipe serial = new PipeSerial (negative, twitter);
+            IPipe end = new PipeNull (); // Pipe donde termina el programa
+            IPipe twitter = new PipeSerial (twitterPub, end); // PipeSerial para publicar en Twitter y llevar la imagen al último Pipe
+            IPipe serial = new PipeSerial (negative, twitter); // PipeSerial para aplicarle el filtro NEGATIVO y pasaje al segundo Pipe 
 
-            img.SavePicture (serial.Send(image), "BreakingBadNegative.jpg");          
+            img.SavePicture (serial.Send(image), "BreakingBadNegative.jpg"); // Guarda la imagen con el nombre que se le adjudica       
 
         }
     }
