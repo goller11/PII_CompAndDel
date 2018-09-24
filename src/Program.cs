65dcd85b5c511614914cbd4339eb0f77baa38eac
Vogel <vogel@github.com>
2018-09-24 18:58:08 -0300

    Twitter deleted by the moment

--- src/Program.cs
+++ src/Program.cs
@@ -14,8 +14,8 @@ namespace CompAndDel {
             IFilter twitterFilter = new TwitterFilter (); //Creación del filtro de Twitter para publicar
 
             IPipe pipeEnd = new PipeNull (); // Pipe donde termina el programa
-            IPipe pipeTwitter = new PipeSerial (twitterPub, pipeEnd); // PipeSerial para publicar en Twitter y llevar la imagen al último Pipe
-            IPipe pipeSerial = new PipeSerial (negativeFilter, pipeTwitter); // PipeSerial para aplicarle el filtro NEGATIVO y pasaje al segundo Pipe 
+            //IPipe pipeTwitter = new PipeSerial (twitterPub, pipeEnd); // PipeSerial para publicar en Twitter y llevar la imagen al último Pipe
+            IPipe pipeSerial = new PipeSerial (negativeFilter, pipeEnd); // PipeSerial para aplicarle el filtro NEGATIVO y pasaje al segundo Pipe 
 
             imgProvider.SavePicture (pipeSerial.Send(pictureP), "BreakingBadNegative.jpg"); // Guarda la imagen con el nombre que se le adjudica       
 
