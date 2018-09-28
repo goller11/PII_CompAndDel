2029bfbfe62252a259c2530b68548133d91e4ddb
Guillermo Vogel <vogel11@users.noreply.github.com>
2018-09-28 11:41:17 -0300

    Trying

--- src/Filters/FilterCognitive.cs
+++ src/Filters/FilterCognitive.cs
@@ -10,15 +10,15 @@ namespace CompAndDel.Filters {
     public class FilterCognitive : IFilterBool {
         CognitiveFace cognitiveFace = new CognitiveFace("6cc93ca750fc4e0b9b716925f303dcc1", Color.GreenYellow);
         public bool FaceOrNot { get; set; }
-        PictureProvider imgProvider = new PictureProvider();
+        PictureProvider imageProvider = new PictureProvider();
         public IPicture Filter(IPicture image) {
 
-            imgProvider.SavePicture (image, "TempBreakingBAD.jpg");
+            imageProvider.SavePicture (image, "TempBreakingBAD.jpg");
             cognitiveFace.Recognize(@"TempBreakingBAD.jpg");
 
             FaceOrNot = cognitiveFace.FaceFound;
 
-            IPicture imagen = imgProvider.GetPicture ("tmpFace.jpg");
+            IPicture imagen = imageProvider.GetPicture ("tmpFace.jpg");
             return imagen;
         }
     }
--- src/IFilterBool.cs
+++ src/IFilterBool.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace CompAndDel.Filters {
     public interface IFilterBool : IFilter {
-        bool FaceOrNot { get; set;}
+        bool FaceOrNot { get;}
 
         IPicture Filter(IPicture image);
     }
