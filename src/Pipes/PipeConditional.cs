e682f4c711cc246940fefd3108e7e67e0291fb39
Guillermo Vogel <guillevogel11@gmail.com>
2018-09-28 01:07:16 -0300

    PipeConditional via Pocket Git

--- src/Pipes/PipeConditional.cs
+++ src/Pipes/PipeConditional.cs
@@ -17,38 +17,30 @@ namespace CompAndDel.Filters.Pipes {
         /// </summary>
         /// <param name="filtro">Filtro que se debe aplicar sobre la imagen</param>
         /// <param name="nextPipe">Siguiente cañería</param>
-        public PipeConditional (IFilter filtro, IPipe nextPipeTrue, IPipe nextPipeFalse) {
-            this.filtro = filtro;
+        public PipeConditional (IFilterBool filtroBool, IPipe nextPipeTrue, IPipe nextPipeFalse) {
+            this.filtroBool = filtroBool;
             this.nextPipeTrue = nextPipeTrue;
             this.nextPipeFalse = nextPipeFalse;
         }
-        /// <summary>
-        /// Devuelve el proximo IPipe
-        /// </summary>
-        public IPipe Next {
-            get {
-                if (this.)
+        
+        public IFilter Filter {
+            get { return this.filtro; }
+        }
 
-                    return this.nextPipeTrue;
+    public IPicture Send(IPicture picture)
+        {
+            picture = this.filtro.Filter(picture);
+
+            if (this.filtro.Boolean)
+            {
+                Console.WriteLine("Face Found! :)");
+                return this.nextPipeTrue.Send(picture);
+            }
+            else
+            {
+                Console.WriteLine("No Face Found :(");
+                return this.nextPipeFalse.Send(picture);
             }
         }
-
-        public IPipe NextFalse {
-            get { return this.nextPipeFalse; }
-        }
-        /// <summary>
-        /// Devuelve el IFilter que aplica este pipe
-        /// </summary>
-        public bool Filter {
-            get { return this.filtroBool; }
-        }
-        /// <summary>
-        /// Recibe una imagen, le aplica un filtro y la envía al siguiente Pipe
-        /// </summary>
-        /// <param name="picture">Imagen a la cual se debe aplicar el filtro</param>
-        public IPicture Send (IPicture picture) {
-            picture = this.filtro.Filter (picture);
-            return this.nextPipe.Send (picture);
-        }
-    }
+}
 }
